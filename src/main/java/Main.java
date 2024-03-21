d8e7e947b7afbd80401de6d8f63b68fcba8c1b53
Tiago Monteiro Alves <101593263+tiagomont@users.noreply.github.com>
2024-03-21 00:00:20 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello world!");
+  }
+}
